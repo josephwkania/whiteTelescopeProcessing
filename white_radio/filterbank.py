8adc1392fcb51656c73d8fce2750d91c629a47a2
josephwkania <60994292+josephwkania@users.noreply.github.com>
2021-01-12 02:11:32 -0500

    filterbank.py py3 compatable. CI tests py3

--- white_radio/filterbank.py
+++ white_radio/filterbank.py
@@ -10,42 +10,45 @@ import warnings
 import os
 import os.path
 import numpy as np
-import sigproc
-#import spectra
+from white_radio import sigproc
+
+# import spectra
 
 
 DEBUG = False
 
-def create_filterbank_file(outfn, header, spectra=None, nbits=8, \
-                           verbose=False, mode='append'):
+
+def create_filterbank_file(
+    outfn, header, spectra=None, nbits=8, verbose=False, mode="append"
+):
     """Write filterbank header and spectra to file.
 
-        Input:
-            outfn: The outfile filterbank file's name.
-            header: A dictionary of header paramters and values.
-            spectra: Spectra to write to file. (Default: don't write
-                any spectra - i.e. write out header only)
-            nbits: The number of bits per sample of the filterbank file.
-                This value always overrides the value in the header dictionary.
-                (Default: 8 - i.e. each sample is an 8-bit integer)
-            verbose: If True, be verbose (Default: be quiet)
-            mode: Mode for writing (can be 'append' or 'write')
+    Input:
+        outfn: The outfile filterbank file's name.
+        header: A dictionary of header paramters and values.
+        spectra: Spectra to write to file. (Default: don't write
+            any spectra - i.e. write out header only)
+        nbits: The number of bits per sample of the filterbank file.
+            This value always overrides the value in the header dictionary.
+            (Default: 8 - i.e. each sample is an 8-bit integer)
+        verbose: If True, be verbose (Default: be quiet)
+        mode: Mode for writing (can be 'append' or 'write')
 
-        Output:
-            fbfile: The resulting FilterbankFile object opened
-                in read-write mode.
+    Output:
+        fbfile: The resulting FilterbankFile object opened
+            in read-write mode.
     """
-    dtype = get_dtype(nbits) # Get dtype. This will check to ensure
-                             # 'nbits' is valid.
-    header['nbits'] = nbits
-    outfile = open(outfn, 'wb')
+    dtype = get_dtype(nbits)  # Get dtype. This will check to ensure
+    # 'nbits' is valid.
+    header["nbits"] = nbits
+    outfile = open(outfn, "wb")
     outfile.write(sigproc.addto_hdr("HEADER_START", None))
     for paramname in header.keys():
         if paramname not in sigproc.header_params:
             # Only add recognized parameters
             continue
         if verbose:
-            print "Writing header param (%s)" % paramname
+            print("Writing header param (%s)" % paramname)
         value = header[paramname]
         outfile.write(sigproc.addto_hdr(paramname, value))
     outfile.write(sigproc.addto_hdr("HEADER_END", None))
@@ -57,16 +60,16 @@ def create_filterbank_file(outfn, header, spectra=None, nbits=8, \
 
 def is_float(nbits):
     """For a given number of bits per sample return
-        true if it corresponds to floating-point samples
-        in filterbank files.
+    true if it corresponds to floating-point samples
+    in filterbank files.
 
-        Input:
-            nbits: Number of bits per sample, as recorded in the filterbank
-                file's header.
+    Input:
+        nbits: Number of bits per sample, as recorded in the filterbank
+            file's header.
 
-        Output:
-            isfloat: True, if 'nbits' indicates the data in the file
-                are encoded as floats.
+    Output:
+        isfloat: True, if 'nbits' indicates the data in the file
+            are encoded as floats.
     """
     check_nbits(nbits)
     if nbits == 32:
@@ -77,66 +80,68 @@ def is_float(nbits):
 
 def check_nbits(nbits):
     """Given a number of bits per sample check to make
-        sure 'filterbank.py' can cope with it.
+    sure 'filterbank.py' can cope with it.
 
-        An exception is raise if 'filterbank.py' cannot cope.
+    An exception is raise if 'filterbank.py' cannot cope.
 
-        Input:
-            nbits: Number of bits per sample, as recorded in the filterbank
-                file's header.
+    Input:
+        nbits: Number of bits per sample, as recorded in the filterbank
+            file's header.
 
-        Output:
-            None
+    Output:
+        None
     """
     if nbits not in [32, 16, 8]:
-        raise ValueError("'filterbank.py' only supports " \
-                                    "files with 8- or 16-bit " \
-                                    "integers, or 32-bit floats " \
-                                    "(nbits provided: %g)!" % nbits)
+        raise ValueError(
+            "'filterbank.py' only supports "
+            "files with 8- or 16-bit "
+            "integers, or 32-bit floats "
+            "(nbits provided: %g)!" % nbits
+        )
 
 
 def get_dtype(nbits):
     """For a given number of bits per sample return
-        a numpy-recognized dtype.
+    a numpy-recognized dtype.
 
-        Input:
-            nbits: Number of bits per sample, as recorded in the filterbank
-                file's header.
+    Input:
+        nbits: Number of bits per sample, as recorded in the filterbank
+            file's header.
 
-        Output:
-            dtype: A numpy-recognized dtype string.
+    Output:
+        dtype: A numpy-recognized dtype string.
     """
     check_nbits(nbits)
     if is_float(nbits):
-        dtype = 'float%d' % nbits
+        dtype = "float%d" % nbits
     else:
-        dtype = 'uint%d' % nbits
+        dtype = "uint%d" % nbits
     return dtype
 
 
 def read_header(filename, verbose=False):
     """Read the header of a filterbank file, and return
-        a dictionary of header paramters and the header's
-        size in bytes.
+    a dictionary of header paramters and the header's
+    size in bytes.
 
-        Inputs:
-            filename: Name of the filterbank file.
-            verbose: If True, be verbose. (Default: be quiet)
+    Inputs:
+        filename: Name of the filterbank file.
+        verbose: If True, be verbose. (Default: be quiet)
 
-        Outputs:
-            header: A dictionary of header paramters.
-            header_size: The size of the header in bytes.
+    Outputs:
+        header: A dictionary of header paramters.
+        header_size: The size of the header in bytes.
     """
     header = {}
-    filfile = open(filename, 'rb')
+    filfile = open(filename, "rb")
     filfile.seek(0)
     paramname = ""
-    while (paramname != 'HEADER_END'):
+    while paramname != "HEADER_END":
         if verbose:
-            print "File location: %d" % filfile.tell()
+            print("File location: %d" % filfile.tell())
         paramname, val = sigproc.read_hdr_val(filfile, stdout=verbose)
         if verbose:
-            print "Read param %s (value: %s)" % (paramname, val)
+            print("Read param %s (value: %s)" % (paramname, val))
         if paramname not in ["HEADER_START", "HEADER_END"]:
             header[paramname] = val
     header_size = filfile.tell()
@@ -145,24 +150,28 @@ def read_header(filename, verbose=False):
 
 
 class FilterbankFile(object):
-    def __init__(self, filfn, mode='readonly'):
+    def __init__(self, filfn, mode="readonly"):
         self.filename = filfn
         self.filfile = None
         if not os.path.isfile(filfn):
             raise ValueError("ERROR: File does not exist!\n\t(%s)" % filfn)
         self.header, self.header_size = read_header(self.filename)
-        self.frequencies = self.fch1 + self.foff*np.arange(self.nchans)
-        self.is_hifreq_first = (self.foff < 0)
-        self.bytes_per_spectrum = self.nchans*self.nbits / 8
-        data_size = os.path.getsize(self.filename)-self.header_size
-        self.nspec = data_size/self.bytes_per_spectrum
-       
+        self.frequencies = self.fch1 + self.foff * np.arange(self.nchans)
+        self.is_hifreq_first = self.foff < 0
+        self.bytes_per_spectrum = self.nchans * self.nbits / 8
+        data_size = os.path.getsize(self.filename) - self.header_size
+        self.nspec = data_size / self.bytes_per_spectrum
+
         # Check if this file is a folded-filterbank file
-        if 'npuls' in self.header and 'period' in self.header and \
-                'nbins' in self.header and 'tsamp' not in self.header:
+        if (
+            "npuls" in self.header
+            and "period" in self.header
+            and "nbins" in self.header
+            and "tsamp" not in self.header
+        ):
             # Foleded file
             self.isfold = True
-            self.dt = self.period/self.nbins
+            self.dt = self.period / self.nbins
         else:
             self.isfold = False
             self.dt = self.tsamp
@@ -176,12 +185,12 @@ class FilterbankFile(object):
         self.dtype_min = tinfo.min
         self.dtype_max = tinfo.max
 
-        if mode.lower() in ('read', 'readonly'):
-            self.filfile = open(self.filename, 'rb')
-        elif mode.lower() in ('write', 'readwrite'):
-            self.filfile = open(self.filename, 'r+b')
-        elif mode.lower() == 'append':
-            self.filfile = open(self.filename, 'a+b')
+        if mode.lower() in ("read", "readonly"):
+            self.filfile = open(self.filename, "rb")
+        elif mode.lower() in ("write", "readwrite"):
+            self.filfile = open(self.filename, "r+b")
+        elif mode.lower() == "append":
+            self.filfile = open(self.filename, "a+b")
         else:
             raise ValueError("Unrecognized mode (%s)!" % mode)
 
@@ -200,9 +209,10 @@ class FilterbankFile(object):
             self.filfile.close()
 
     def get_timeslice(self, start, stop):
-        startspec = int(np.round(start/self.tsamp))
-        stopspec = int(np.round(stop/self.tsamp))
-        return self.get_spectra(startspec, stopspec-startspec)
+        startspec = int(np.round(start / self.tsamp))
+        stopspec = int(np.round(stop / self.tsamp))
+        return self.get_spectra(startspec, stopspec - startspec)
+
     """
     def get_spectra(self, start, nspec):
         stop = min(start+nspec, self.nspec)
@@ -219,86 +229,91 @@ class FilterbankFile(object):
                 starttime=start*self.tsamp, dm=0.0)
         return spec
     """
+
     def append_spectra(self, spectra):
         """Append spectra to the file if is not read-only.
-            
-            Input:
-                spectra: The spectra to append. The new spectra
-                    must have the correct number of channels (ie
-                    dimension of axis=1.
 
-            Outputs:
-                None
+        Input:
+            spectra: The spectra to append. The new spectra
+                must have the correct number of channels (ie
+                dimension of axis=1.
+
+        Outputs:
+            None
         """
-        if self.filfile.mode.lower() in ('r', 'rb'):
-            raise ValueError("FilterbankFile object for '%s' is read-only." % \
-                        self.filename)
+        if self.filfile.mode.lower() in ("r", "rb"):
+            raise ValueError(
+                "FilterbankFile object for '%s' is read-only." % self.filename
+            )
         nspec, nchans = spectra.shape
         if nchans != self.nchans:
-            raise ValueError("Cannot append spectra. Incorrect shape. " \
-                        "Number of channels in file: %d; Number of " \
-                        "channels in spectra to append: %d" % \
-                        (self.nchans, nchans))
+            raise ValueError(
+                "Cannot append spectra. Incorrect shape. "
+                "Number of channels in file: %d; Number of "
+                "channels in spectra to append: %d" % (self.nchans, nchans)
+            )
         data = spectra.flatten()
         np.clip(data, self.dtype_min, self.dtype_max, out=data)
         # Move to end of file
         self.filfile.seek(0, os.SEEK_END)
         self.filfile.write(data.astype(self.dtype))
         self.nspec += nspec
-        #self.filfile.flush()
-        #os.fsync(self.filfile)
+        # self.filfile.flush()
+        # os.fsync(self.filfile)
 
     def write_spectra(self, spectra, ispec):
         """Write spectra to the file if is writable.
-            
-            Input:
-                spectra: The spectra to append. The new spectra
-                    must have the correct number of channels (ie
-                    dimension of axis=1.
-                ispec: The index of the spectrum of where to start writing.
 
-            Outputs:
-                None
+        Input:
+            spectra: The spectra to append. The new spectra
+                must have the correct number of channels (ie
+                dimension of axis=1.
+            ispec: The index of the spectrum of where to start writing.
+
+        Outputs:
+            None
         """
-        if 'r+' not in self.filfile.mode.lower():
-            raise ValueError("FilterbankFile object for '%s' is not writable." % \
-                        self.filename)
+        if "r+" not in self.filfile.mode.lower():
+            raise ValueError(
+                "FilterbankFile object for '%s' is not writable." % self.filename
+            )
         nspec, nchans = spectra.shape
         if nchans != self.nchans:
-            raise ValueError("Cannot write spectra. Incorrect shape. " \
-                        "Number of channels in file: %d; Number of " \
-                        "channels in spectra to write: %d" % \
-                        (self.nchans, nchans))
+            raise ValueError(
+                "Cannot write spectra. Incorrect shape. "
+                "Number of channels in file: %d; Number of "
+                "channels in spectra to write: %d" % (self.nchans, nchans)
+            )
         if ispec > self.nspec:
-            raise ValueError("Cannot write past end of file! " \
-                             "Present number of spectra: %d; " \
-                             "Requested index of write: %d" % \
-                             (self.nspec, ispec))
+            raise ValueError(
+                "Cannot write past end of file! "
+                "Present number of spectra: %d; "
+                "Requested index of write: %d" % (self.nspec, ispec)
+            )
         data = spectra.flatten()
         np.clip(data, self.dtype_min, self.dtype_max, out=data)
         # Move to requested position
-        pos = self.header_size + ispec*self.bytes_per_spectrum
+        pos = self.header_size + ispec * self.bytes_per_spectrum
         self.filfile.seek(pos, os.SEEK_SET)
         self.filfile.write(data.astype(self.dtype))
-        if nspec+ispec > self.nspec:
-            self.nspec = nspec+ispec
+        if nspec + ispec > self.nspec:
+            self.nspec = nspec + ispec
 
     def __getattr__(self, name):
         if name in self.header:
             if DEBUG:
-                print "Fetching header param (%s)" % name
+                print("Fetching header param (%s)" % name)
             val = self.header[name]
         else:
             raise ValueError("No FilterbankFile attribute called '%s'" % name)
         return val
 
     def print_header(self):
-        """Print header parameters and values.
-        """
+        """Print header parameters and values."""
         for param in sorted(self.header.keys()):
             if param in ("HEADER_START", "HEADER_END"):
                 continue
-            print "%s: %s" % (param, self.header[param])
+            print("%s: %s" % (param, self.header[param]))
 
 
 def main():
@@ -306,5 +321,5 @@ def main():
     fil.print_header()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
